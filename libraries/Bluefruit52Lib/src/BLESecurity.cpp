1ff67a2d1acf4674b835c74331cc68c97fdca3fd
Bennett Ramirez <bramirez@mineralanalytics.com>
2023-08-22 17:57:40 -0500

    Add more guards for BLE central-only code

--- libraries/Bluefruit52Lib/src/BLESecurity.cpp
+++ libraries/Bluefruit52Lib/src/BLESecurity.cpp
@@ -239,10 +239,15 @@ bool BLESecurity::_authenticate(uint16_t conn_hdl)
 
 bool BLESecurity::_encrypt(uint16_t conn_hdl, bond_keys_t const* ltkey)
 {
+#ifdef BLE_GAP_ROLE_CENTRAL
   // LESC use own key, Legacy use peer key
   ble_gap_enc_key_t const* enc_key = ltkey->own_enc.enc_info.lesc ? &ltkey->own_enc : &ltkey->peer_enc;
   VERIFY_STATUS(sd_ble_gap_encrypt(conn_hdl, &enc_key->master_id, &enc_key->enc_info), false);
   return true;
+#else
+  // This SoftDevice doesn't support this function
+  return false;
+#endif
 }
 
 //--------------------------------------------------------------------+
